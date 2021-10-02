662e92f523ddb2b0dcc92a30382ea243c79b0a19
Rishabh Singh <ris8645208@gmail.com>
2021-10-02 22:19:19 +0530

    feat: add ejs views templates and rendered on routes

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ app.set("view engine", "ejs");
 app.listen(process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
 // redirects
@@ -25,5 +23,5 @@ app.get("/about-us", (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+    res.render("404");
 });
